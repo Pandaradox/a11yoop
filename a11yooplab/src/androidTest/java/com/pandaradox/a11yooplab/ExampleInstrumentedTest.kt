472befabb5165780b5e08389471d32339a8a8a7b
JCardar1 <john.cardarella1@t-mobile.com>
2023-02-24 15:55:20 -0600

    Testing UI test fail

--- a11yooplab/src/androidTest/java/com/pandaradox/a11yooplab/ExampleInstrumentedTest.kt
+++ a11yooplab/src/androidTest/java/com/pandaradox/a11yooplab/ExampleInstrumentedTest.kt
@@ -20,5 +20,6 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.pandaradox.a11yooplab", appContext.packageName)
+        assert(false)
     }
 }
